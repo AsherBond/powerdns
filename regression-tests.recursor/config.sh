699d166c0a2ae3e3cb95db12d9a901a153dc9f67
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2012-06-01 15:17:32 +0200

    actually hook up the lua-prequery scripts

--- regression-tests.recursor/config.sh
+++ regression-tests.recursor/config.sh
@@ -52,7 +52,7 @@ ns.arthur.example.net.   3600 IN A   $PREFIX.12
 ns2.arthur.example.net.  3600 IN A   $PREFIX.13
 prefect.example.net.     3600 IN NS  ns.prefect.example.net.
 ns.prefect.example.net.  3600 IN A   $PREFIX.14
-marvin.example.net.      3600 IN NS  ns.prefect.example.net.
+marvin.example.net.      3600 IN NS  ns.marvin.example.net.
 ns.marvin.example.net.   3600 IN A   $PREFIX.15
 trillian.example.net.    3600 IN NS  ns.trillian.example.net.
 ns.trillian.example.net. 3600 IN A   $PREFIX.16
@@ -156,6 +156,11 @@ bind-config=named.conf
 no-shuffle
 socket-dir=.
 EOF
+
+    if [ -e $dir/prequery.lua ]
+    then
+        echo 'lua-prequery-script=prequery.lua' >> $dir/pdns.conf
+    fi
     
     cat > $dir/named.conf <<EOF
 options {
